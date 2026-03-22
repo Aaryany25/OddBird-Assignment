25e29b77a4b92aee9627b58681de95a74058ff97
Aryan Yadav <aaryany2505@gmail.com>
2026-03-22 19:56:09 +0530

    Hero Section Completed

--- components/HeroCard.tsx
+++ components/HeroCard.tsx
@@ -1,15 +1,17 @@
-// type Content={
-//     index:number,
-//     title:string,
-//     desc:string
-// }
-export default function HeroCard(){
+"use client"
+
+type Content={
+    index:number,
+    title:string,
+    desc:string
+}
+export default function HeroCard({index,title,desc}:Content){
     return(
 <div className="w-full bg-[#E2E2E0] py-10 px-5 border-b-2 border-gray-500
 ">
-<h3 className="text-xl">1</h3>
-<h1 className="text-4xl font-light my-2">Product Design & MVPs</h1>
-<p className="text-gray-500 text-lg">FullStack App, Native Apps ,Staff Augmentation</p>
+<h3 className="text-xl">{index}</h3>
+<h1 className="text-4xl font-light my-2">{title}</h1>
+<p className="text-gray-500 text-lg">{desc}</p>
 </div>
     )
 }
--- components/sections/HeroRight.tsx
+++ components/sections/HeroRight.tsx
@@ -1,12 +1,28 @@
+"use client"
 import HeroCard from "../HeroCard";
 
+const data=[{
+    id:1,
+    title:"Product Design and MVPs",
+    desc:"Full Stack , Native Apps ,Staff Augmentation"
+},{
+    id:2,
+title:"Greenflied Innovation",
+    desc:"Full Stack , Native Apps ,Staff Augmentation"
+},{
+    id:3,
+title:"Sophatiscated Websites",
+    desc:"Full Stack , Native Apps ,Staff Augmentation"
+}]
 export default function HeroRight(){
     return(
         <div className="w-full h-full flex flex-col justify-between py-10 px-5">
-
+{
+    data.map(card=><HeroCard key={card.id} index={card.id} title={card.title} desc={card.desc}/>)
+}
+            {/* <HeroCard/>
             <HeroCard/>
-            <HeroCard/>
-            <HeroCard/>
+            <HeroCard/> */}
 
         </div>
     )
